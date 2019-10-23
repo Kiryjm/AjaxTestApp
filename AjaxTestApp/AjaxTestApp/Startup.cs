082d816add807170ba2429e55f939970b44f10e8
Kiryjm <kiryjm@gmail.com>
2019-10-24 01:50:14 +0300

    ASP .NET MVC application using books store model database MS SQL and jQuery approach to establish AJAX requests

---
+++ AjaxTestApp/AjaxTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AjaxTestApp.Startup))]
+namespace AjaxTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
